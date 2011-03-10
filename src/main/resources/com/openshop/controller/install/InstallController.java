0416d5dcd041d3d0eec240148f7e1ab3d4a12b33
patrick.trautmann@gmail.com <patrick.trautmann@gmail.com@3e4039f6-7fef-17e9-bba7-33956c923aed>
2011-03-10 14:49:50 +0000

    Started Developing Installation Step 2: Database Settings

--- src/main/resources/com/openshop/controller/install/InstallController.java
+++ src/main/resources/com/openshop/controller/install/InstallController.java
@@ -3,6 +3,7 @@ package com.openshop.controller.install;
 import com.openshop.beans.install.InstallSettings;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -12,13 +13,25 @@ import javax.faces.bean.SessionScoped;
 public class InstallController {
 
     private final InstallSettings settings = new InstallSettings();
+    private PropertiesConfiguration config;
+
+    public InstallController() {
+
+        try {
+            config = new PropertiesConfiguration("config.properties");
+            config.setReloadingStrategy(new FileChangedReloadingStrategy());
+
+            setDefaultInstallSettings();
+        } catch (Exception e) {
+            e.printStackTrace(); //todo: Exception Handling
+        }
+
+    }
 
     public String setLanguage() {
 
-        PropertiesConfiguration config = null;
         try {
 
-            config = new PropertiesConfiguration("config.properties");
             config.setProperty("locale", getSettings().getLanguageSelection());
             config.save();
 
@@ -27,6 +40,27 @@ public class InstallController {
         }
 
         return "stepDatabase";
+    }
+
+    public String setDatabase() {
+
+        //ToDo: Finish it :)
+        if (getSettings().getDbDBMS().equals("mysql")) {
+            config.setProperty("database.dbms", getSettings().getDbDBMS());
+        }
+        //...
+
+        return "stepAdministrator";
+    }
+
+    private void setDefaultInstallSettings() {
+
+        getSettings().setDbDBMS(config.getString("database.dbms"));
+        getSettings().setDbHostname(config.getString("database.hostname"));
+        getSettings().setDbUsername(config.getString("database.username"));
+        getSettings().setDbPassword(config.getString("database.password"));
+        getSettings().setDbSchema(config.getString("database.schema"));
+
     }
 
     public InstallSettings getSettings() {
