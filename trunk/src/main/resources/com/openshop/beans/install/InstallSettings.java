ae3f5c58580283ffb7cf3d9658112cd681cafc5c
patrick.trautmann@gmail.com <patrick.trautmann@gmail.com@3e4039f6-7fef-17e9-bba7-33956c923aed>
2011-03-10 14:49:50 +0000

    Started Developing Installation Step 2: Database Settings

--- trunk/src/main/resources/com/openshop/beans/install/InstallSettings.java
+++ trunk/src/main/resources/com/openshop/beans/install/InstallSettings.java
@@ -4,6 +4,11 @@ package com.openshop.beans.install;
 public class InstallSettings {
 
     private String languageSelection;
+    private String dbHostname;
+    private String dbUsername;
+    private String dbPassword;
+    private String dbDBMS;
+    private String dbSchema;
 
     public InstallSettings() {
     }
@@ -14,5 +19,45 @@ public class InstallSettings {
 
     public void setLanguageSelection(String languageSelection) {
         this.languageSelection = languageSelection;
+    }
+
+    public String getDbHostname() {
+        return dbHostname;
+    }
+
+    public void setDbHostname(String dbHostname) {
+        this.dbHostname = dbHostname;
+    }
+
+    public String getDbUsername() {
+        return dbUsername;
+    }
+
+    public void setDbUsername(String dbUsername) {
+        this.dbUsername = dbUsername;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
+
+    public void setDbPassword(String dbPassword) {
+        this.dbPassword = dbPassword;
+    }
+
+    public String getDbDBMS() {
+        return dbDBMS;
+    }
+
+    public void setDbDBMS(String dbDBMS) {
+        this.dbDBMS = dbDBMS;
+    }
+
+    public String getDbSchema() {
+        return dbSchema;
+    }
+
+    public void setDbSchema(String dbSchema) {
+        this.dbSchema = dbSchema;
     }
 }
