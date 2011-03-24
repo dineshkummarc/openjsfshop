ed68f44fa7131e724f6326f798d2edb3bcf80350
patrick.trautmann@gmail.com <patrick.trautmann@gmail.com@3e4039f6-7fef-17e9-bba7-33956c923aed>
2011-03-24 21:36:24 +0000

    Some minimal Admin-CP Progress

---
+++ src/main/resources/com/openshop/controller/admincp/LoginController.java
@@ -0,0 +1,17 @@
+package com.openshop.controller.admincp;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LoginController {
+
+    /**
+     * EventHandler for Login Button at login.xhtml
+     *
+     * @return if success = "dashboard" else null
+     */
+    public String buttonLogIn() {
+        return null;
+    }
+
+}
